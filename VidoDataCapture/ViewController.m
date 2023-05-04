ac794f135d0179b8ed494c10894f12dc05d13420
liqiang <liqiang@orvibo.com>
2023-05-04 14:49:46 +0800

    保存h264文件

--- VidoDataCapture/ViewController.m
+++ VidoDataCapture/ViewController.m
@@ -23,21 +23,43 @@
 
 @property (assign, nonatomic) VTCompressionSessionRef compressionSessionRef;
 
+
+@property (strong, nonatomic) NSFileHandle * fileHandle;
+
 @end
+
+
+static ViewController * objc;
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    [self inInit];
-    TestView * view = [[TestView alloc] initWithFrame:CGRectMake(50, 50, 100, 100)];
-    view.backgroundColor = [UIColor redColor];
-    [self.view addSubview:view];
+    objc = self;
+    NSString * fileName = [NSString stringWithFormat:@"%ld",[[NSDate date] timeIntervalSince1970]];
+    
+     NSString *homePath  = NSHomeDirectory();
+    NSString *sourcePath = [NSHomeDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"Documents/%@.h264",fileName]];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if(![[NSFileManager defaultManager] createFileAtPath:sourcePath contents:nil attributes:nil]) {
+        
+    }
+     _fileHandle= [NSFileHandle fileHandleForWritingAtPath:sourcePath];
+//     [self.fileHandle writeData:stringData];
+//     [self.fileHandle closeFile];
+    
+    
+    [self inInit];
+//    TestView * view = [[TestView alloc] initWithFrame:CGRectMake(50, 50, 100, 100)];
+//    view.backgroundColor = [UIColor redColor];
+//    [self.view addSubview:view];
     
     // Do any additional setup after loading the view.
 }
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    
+    [self.captrueSession stopRunning];
+    [self stopVideoEncode];
+    [self.fileHandle closeFile];
     
 }
 - (void)inInit {
@@ -48,6 +70,8 @@
     [self updateFps:20];
     
   
+    [self createEncoder];
+//    [self startVideoEncode];
     
 }
 - (void)createEncoder {
@@ -90,7 +114,7 @@
         return;
     }
     
-    //I帧产生时间间隔
+    //每个几个帧创建一个关键帧 100 表示每隔99帧创建一个关键帧
     status = VTSessionSetProperty(_compressionSessionRef, kVTCompressionPropertyKey_MaxKeyFrameInterval, (__bridge CFTypeRef)@(15*240));
     if (noErr != status)
     {
@@ -145,7 +169,85 @@
     return YES;
 }
 void encodeOutPutDataCallback(void * CM_NULLABLE outputCallbackRefCon,void * CM_NULLABLE soureFrmeRefCon,OSStatus status,VTEncodeInfoFlags infoFlags,CM_NULLABLE CMSampleBufferRef sampleBuffer) {
+    if(noErr != status || nil == sampleBuffer) {
+        NSLog(@"VEVideoEncoder::encodeOutputCallback Error : %d!", (int)status);
+        return;
+    }
+    if(nil == outputCallbackRefCon) {
+        NSLog(@"outputCallbackRefCon = nil");
+        return;
+    }
+    if(!CMSampleBufferDataIsReady(sampleBuffer)) {
+        NSLog(@"!CMSampleBufferDataIsReady(sampleBuffer)");
+        return;
+    }
+    ViewController *encoder = (__bridge ViewController *)outputCallbackRefCon;
+
+    const char header[] = "\x00\x00\x00\x01";
+    size_t headerLen = sizeof(header) - 1;
+    NSData * headerData = [NSData dataWithBytes:header length:headerLen];
+    bool isKeyFrame = !CFDictionaryContainsKey(CFArrayGetValueAtIndex(CMSampleBufferGetSampleAttachmentsArray(sampleBuffer, YES), 0), (const void*)kCMSampleAttachmentKey_NotSync);
+    if(isKeyFrame) {
+        NSLog(@"VEVideoEncoder::编码了一个关键帧");
+        CMFormatDescriptionRef formateDescriptionRef = CMSampleBufferGetFormatDescription(sampleBuffer);
+        
+        size_t sParameterSetSize,sParameterSetCount;
+        const uint8_t * sParameterSet;
+        OSStatus spsStatus = CMVideoFormatDescriptionGetH264ParameterSetAtIndex(formateDescriptionRef, 0, &sParameterSet, &sParameterSetSize, &sParameterSetCount, 0);
+        
+        size_t pParameterSetSize,pParameterSetCount;
+        const uint8_t *pParameterSet;
+        OSStatus ppsStatus = CMVideoFormatDescriptionGetH264ParameterSetAtIndex(formateDescriptionRef, 1, &pParameterSet, &pParameterSetSize, &pParameterSetCount, 0);
+        
+        if(spsStatus == noErr && ppsStatus == noErr) {
+            NSData * sps = [NSData dataWithBytes:sParameterSet length:sParameterSetSize];
+            NSMutableData * spsData = [NSMutableData data];
+            [spsData appendData:headerData];
+            [spsData appendData:sps];
+//            [self wite]
+            [encoder writeDataToFile:spsData];
+            NSData * pps = [NSData dataWithBytes:pParameterSet length:pParameterSetSize];
+            NSMutableData * ppsData = [NSMutableData data];
+            [ppsData appendData:headerData];
+            [ppsData appendData:pps];
+            [encoder writeDataToFile:spsData];
+        }
+        
+        
+    }else {
+        NSLog(@"VEVideoEncoder::编码了一个非非非关键帧");
+
+    }
     
+    CMBlockBufferRef blockBuffer = CMSampleBufferGetDataBuffer(sampleBuffer);
+    size_t length,totalLength;
+    char * dataPointer;
+    status = CMBlockBufferGetDataPointer(blockBuffer, 0, &length, &totalLength, &dataPointer);
+    
+    if(status != noErr) {
+        NSLog(@"VEVideoEncoder::CMBlockBufferGetDataPointer Error : %d!", (int)status);
+        return;
+    }
+    
+    size_t bufferOffset = 0;
+    static const int avcHeaderLength  = 4;
+    while (bufferOffset < totalLength - avcHeaderLength) {
+        uint32_t nalUnitLength = 0;
+        memcpy(&nalUnitLength, dataPointer+bufferOffset, avcHeaderLength);
+        nalUnitLength = CFSwapInt32BigToHost(nalUnitLength);
+        NSData * frameData = [[NSData alloc] initWithBytes:(dataPointer + bufferOffset+avcHeaderLength) length:nalUnitLength];
+        NSMutableData * outPutFrameData = [NSMutableData data];
+        [outPutFrameData appendData:headerData];
+        [outPutFrameData appendData:frameData];
+        [encoder writeDataToFile:outPutFrameData];
+        bufferOffset += avcHeaderLength+nalUnitLength;
+        
+    }
+    
+}
+- (void)writeDataToFile:(NSMutableData *)data{
+    [_fileHandle seekToEndOfFile];
+    [_fileHandle writeData:data];
 }
 - (BOOL)adjustBitRate:(NSInteger)bitRate {
     if (bitRate <= 0)
@@ -245,8 +347,31 @@ void encodeOutPutDataCallback(void * CM_NULLABLE outputCallbackRefCon,void * CM_
 
 - (void)captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
     
-  
+    if(output == self.videoDataOutput) {
+        [self videoEncodeInputData:sampleBuffer forceKeyFrame:NO];
+    }
     
 }
+
+- (BOOL)videoEncodeInputData:(CMSampleBufferRef)sampleBuffer forceKeyFrame:(BOOL)forceKeyFrame{
+    
+    if(_compressionSessionRef == NULL) {
+        return NO;
+    }
+    
+    if(sampleBuffer == nil) {
+        return NO;
+    }
+    CVImageBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    NSDictionary * frameProperites = @{(__bridge NSString *)kVTEncodeFrameOptionKey_ForceKeyFrame:@(forceKeyFrame)};
+    OSStatus status = VTCompressionSessionEncodeFrame(_compressionSessionRef, pixelBuffer, kCMTimeInvalid, kCMTimeInvalid, (__bridge CFDictionaryRef)frameProperites, NULL, NULL);
+    if (noErr != status)
+    {
+        NSLog(@"VEVideoEncoder::VTCompressionSessionEncodeFrame failed! status:%d", (int)status);
+        return NO;
+    }
+    return YES;
+}
+
 
 @end
